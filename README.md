09f451fba3b5404c0cc8e2a41f47af197dc9df71
Max <maxs987@gmail.com>
2017-10-04 15:45:13 +0300

    preparing to upload to PyPi

---
+++ README.md
@@ -0,0 +1,2 @@
+A small logger that lets you write logs readable by Tensorboard but 
+doesn't require Tensorflow.
---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='tensorboard-easy',
+    packages=['tensorboard-easy'],
+    version='0.1',
+    description='A tensorflow-independent tensorboard logger',
+    author='maxme1',
+    author_email='maxs987@gmail.com',
+    url='https://github.com/maxme1/tensorboard_logger',
+    download_url='https://github.com/maxme1/tensorboard_logger/archive/0.1.tar.gz',
+    keywords=['tensorboard', 'logging'],
+    classifiers=[],
+)
