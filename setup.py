5bd0fa3fa8b44a9fa5970c0cb2057246a344c147
Max <maxs987@gmail.com>
2017-10-04 20:59:49 +0300

    added gitignore

--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@ from distutils.core import setup
 setup(
     name='tensorboard-easy',
     packages=['tensorboard_easy'],
-    version='0.1.6',
+    include_package_data=True,
+    version='0.1',
     description='A tensorflow-independent tensorboard logger',
     author='maxme1',
     author_email='maxs987@gmail.com',
     license='MIT',
-    url='https://github.com/maxme1/tensorboard_logger',
-    download_url='https://github.com/maxme1/tensorboard_logger/archive/0.1.6.tar.gz',
+    url='https://github.com/maxme1/tensorboard-easy',
+    download_url='https://github.com/maxme1/tensorboard-easy/archive/0.1.tar.gz',
     keywords=[
         'tensorboard', 'logging'
     ],
